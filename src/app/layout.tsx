f295fd57d4ca773829df08c92a0f69fa3bb91cb0
Chaesunbak <coanstjd0422@gmail.com>
2025-01-10 16:15:45 +0900

    fix : style 폴더 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+import '@/styles/globals.css'
 import { server } from '@/mocks/worker'
 
 // 개발 환경에서만 MSW 서버를 시작합니다.
