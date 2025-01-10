9c4af25eafb66196a7870691be69339965821345
Chaesunbak <coanstjd0422@gmail.com>
2025-01-10 14:37:06 +0900

    chore : 핸들러 작성 요령 주석 handlers.ts에 파일에 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from 'msw'
 
+// 핸들러 예시 코드입니다. 실제 코드 작성시에는 userHandler.ts 같이 새 파일을 만들어서 작성하는 것을 권장합니다.
 export const handlers = [
   // Intercept "GET https://example.com/user" requests...
   http.get('https://example.com/user', () => {
