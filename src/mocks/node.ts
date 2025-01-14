49b950bd34e5261e416d57d375fc35140a1de3c0
Chaesunbak <coanstjd0422@gmail.com>
2025-01-14 17:51:27 +0900

    chore : 리뷰 모킹 API worker로 변경

--- src/mocks/node.ts
+++ src/mocks/node.ts
@@ -1,5 +1,4 @@
 import { setupServer } from 'msw/node'
 import { handlers } from './handlers'
-import { reviewsHandlers } from '@/mocks/reviews-handler'
 
-export const server = setupServer(...handlers, ...reviewsHandlers)
+export const server = setupServer(...handlers)
