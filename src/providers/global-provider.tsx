393e191d45b5a999571305f89c1db214c5be22cb
Chaesunbak <coanstjd0422@gmail.com>
2025-01-14 11:08:49 +0900

    chore : 리액트 쿼리 프로바이더 구현 및 추가

--- src/providers/global-provider.tsx
+++ src/providers/global-provider.tsx
@@ -1,12 +1,17 @@
 import { ReactNode } from 'react'
 import { CounterStoreProvider } from '@/providers/counter-store-provider'
+import { ReactQueryProvider } from '@/providers/react-query-provider'
 
 interface GlobalProviderProps {
   children: ReactNode
 }
 
 const GlobalProvider = ({ children }: GlobalProviderProps) => {
-  return <CounterStoreProvider>{children}</CounterStoreProvider>
+  return (
+    <ReactQueryProvider>
+      <CounterStoreProvider>{children}</CounterStoreProvider>
+    </ReactQueryProvider>
+  )
 }
 
 export default GlobalProvider
