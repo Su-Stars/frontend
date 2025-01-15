e8e03b35d7e2fb576f0ef185deb1325057b5e426
dongguJeong <zzzjdg@naver.com>
2025-01-15 20:47:55 +0900

    feat : dialog 추가

--- src/components/pages/home-page.tsx
+++ src/components/pages/home-page.tsx
@@ -1,18 +1,38 @@
 import KaKaoMap from '../home/KaKaoMap'
-import { Button } from '@/components/ui/button'
 import { ChevronDown, MapPin } from 'lucide-react'
 import { Input } from '@/components/ui/input'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Button } from '../ui/button'
 
 export default function HomePage() {
   return (
     <div className="flex flex-col gap-3">
       <h2 className="text-2xl font-bold">인근 수영장</h2>
       <KaKaoMap />
-      <Button className="w-fit bg-gray-500">
-        <MapPin />
-        서울
-        <ChevronDown />
-      </Button>
+
+      <Dialog>
+        <DialogTrigger asChild className="w-fit bg-gray-500">
+          <Button>
+            <MapPin />
+            서울
+            <ChevronDown />
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>지역으로 검색</DialogTitle>
+            <DialogDescription>지역으로 검색</DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
       <Input placeholder="명칭으로 검색하기" />
       <h2 className="text-2xl font-bold">
         검색 결과 <span className="text-theme">8 </span>개
