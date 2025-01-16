af1c9dfcce914406a7f0bf3ad3451586816c175d
Chaesunbak <coanstjd0422@gmail.com>
2025-01-16 15:59:52 +0900

    chore : 프리텐타드 폰트 설정 주석 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,9 +22,21 @@ const geistMono = Geist_Mono({
 
 const pretendard = localFont({
   src: '../static/fonts/PretendardVariable.woff2',
-  display: 'swap',
-  weight: '45 920',
+  display: 'swap', // 폰트 로딩 중 시스템 폰트로 대체(디폴트 swap)
+  preload: true, // 폰트 미리 로드(디폴트 true)
+  fallback: [
+    // 폰트 로드 실패시 대체 폰트
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'system-ui',
+    'Roboto',
+    'Helvetica Neue',
+    'Arial',
+    'sans-serif',
+  ],
+  weight: '45 920', // Pretendard 가변 폰트 weight 범위
   variable: '--font-pretendard',
+  adjustFontFallback: 'Arial', // 폰트 메트릭 조정을 위한 기준 폰트(디폴트 Arial)
 })
 
 export const metadata: Metadata = {
