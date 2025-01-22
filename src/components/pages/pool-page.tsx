76dc9efededf931aeee0300baded0fad3a957e78
Chaesunbak <coanstjd0422@gmail.com>
2025-01-22 15:10:17 +0900

    feat : 리뷰 프리뷰 컴포넌트 레이지 로딩 적용

--- src/components/pages/pool-page.tsx
+++ src/components/pages/pool-page.tsx
@@ -4,7 +4,10 @@ import PoolDetail from '../pool/pool-detail'
 import PoolKaKaoMap from '../pool/pool-kakao-map'
 import { usePool } from '@/hooks/usePool'
 import { Skeleton } from '../ui/skeleton'
-import ReviewsPage from '@/components/pages/reviews-page'
+import { Suspense, lazy } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+
+const ReviewsPage = lazy(() => import('@/components/pages/reviews-page'))
 
 interface PoolPageParams {
   poolId: string
@@ -40,7 +43,13 @@ export default function PoolPage({ poolId }: PoolPageParams) {
   return (
     <div className="flex flex-col space-y-10">
       <PoolDetail pool={pool} />
-      <ReviewsPage preview poolId={poolId} />
+      <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        <Suspense
+          fallback={<div className="flex flex-col gap-2">리뷰 로딩</div>}
+        >
+          <ReviewsPage preview poolId={poolId} />
+        </Suspense>
+      </ErrorBoundary>
       <PoolKaKaoMap pool={pool} />
     </div>
   )
