916ebfa669982f0c4ec1ea8c37c1784ca2d34678
Chaesunbak <coanstjd0422@gmail.com>
2025-01-25 01:28:45 +0900

    feat : 공통 메타데이터 추가

--- src/app/(menu)/pools/[id]/page.tsx
+++ src/app/(menu)/pools/[id]/page.tsx
@@ -6,6 +6,41 @@ import {
 import PoolPage from '@/components/pages/pool-page'
 import { getPool } from '@/actions/get-pool'
 import { Pool as IPool } from '@/hooks/useSearch'
+import type { Metadata, ResolvingMetadata } from 'next'
+
+type Props = {
+  params: Promise<{ id: string }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  // read route params
+  const id = (await params).id
+
+  // fetch data
+  const response = await fetch(`https://nest-aws.site/api/v1/pools/${id}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  }).then((res) => res.json())
+
+  const pool = response.data[0]
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: pool.name,
+    //TODO 후에 이미지를 추가합니다.
+    openGraph: {
+      images: [...previousImages],
+    },
+  }
+}
 
 export default async function Pool({
   params,
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,90 @@ import GlobalProvider from '@/providers/global-provider'
 import localFont from 'next/font/local'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { Toaster } from '@/components/ui/toaster'
+import type { Metadata } from 'next'
 
 // 개발 환경에서만 MSW 서버를 시작합니다.
 if (process.env.NODE_ENV === 'development') {
   server.listen()
+}
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme',
+    default: '어푸!',
+  },
+  description: '어푸!는 전국 수영인들을 위해 수영장 정보를 제공합니다.',
+  generator: '어푸!',
+  applicationName: '어푸!',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['수영장', '수영장 정보', '자유수영', '자유수영 정보'],
+  authors: [{ name: '어푸!', url: 'https://github.com/Su-Stars' }],
+  creator: '어푸!',
+  publisher: '어푸!',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    title: '어푸!',
+    description: '어푸!는 전국 수영인들을 위해 수영장 정보를 제공합니다.',
+    url: 'https://apuu.netlify.app/',
+    siteName: '어푸!',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png', // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    icon: '/icon.png',
+    shortcut: '/shortcut-icon.png',
+    apple: '/apple-icon.png',
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: '/apple-touch-icon-precomposed.png',
+    },
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '어푸!',
+    description: '어푸!는 전국 수영인들을 위해 수영장 정보를 제공합니다.',
+    siteId: '1467726470533754880',
+    creator: '어푸!',
+    creatorId: '1467726470533754880',
+    images: ['https://nextjs.org/og.png'], // Must be an absolute URL
+  },
+  verification: {
+    google: 'FGxuIZJK4nXKhDlSTwR1EoYjC_pXFrtGDMVYpTuemb8',
+    // other: {
+    //   me: ['my-email', 'my-link'],
+    // },
+  },
+  category: 'information',
 }
 
 const geistSans = Geist({
