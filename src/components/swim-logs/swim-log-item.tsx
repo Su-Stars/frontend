13b6a9718b9d88c3d97742b71ca4ad39a305f281
Chaesunbak <coanstjd0422@gmail.com>
2025-01-26 18:35:28 +0900

    fix : 수영기록 아이템 스타일 개선

--- src/components/swim-logs/swim-log-item.tsx
+++ src/components/swim-logs/swim-log-item.tsx
@@ -54,7 +54,7 @@ export default function SwimLogItem({
     <div className="flex flex-col space-y-2">
       <div className="flex items-center justify-between">
         <h3 className="font-pretendard text-xl font-semibold">
-          {index + 1}번째 수영 기록
+          {index + 1}회차 수영
         </h3>
         <Button
           variant="ghost"
@@ -72,31 +72,29 @@ export default function SwimLogItem({
             {/* 생성일 포맷팅 */}
             {log.created_at && (
               <span>
-                {format(parseISO(log.created_at), 'yyyy년 MM월 dd일', {
+                {format(parseISO(log.created_at), 'yy.MM.dd', {
                   locale: ko,
                 })}
               </span>
             )}
 
             {/* 시작 시간과 종료 시간이 있는 경우에만 표시 */}
-            {log.startTime && (
-              <span className="ml-2">시작: {log.startTime}</span>
-            )}
+            {log.startTime && <span className="ml-2">{log.startTime} ~</span>}
 
-            {log.endTime && <span className="ml-2">종료: {log.endTime}</span>}
-
-            {log.swimCategory && (
-              <span className="ml-2">{log.swimCategory}</span>
-            )}
+            {log.endTime && <span className="ml-2">{log.endTime}</span>}
           </div>
           <div className="flex gap-1 text-3xl font-bold">
             <span className="">{log.swimLength}</span>
             <span className="text-green-500">m</span>
           </div>
+          <div className="text-sm text-gray-500">
+            {log.swimCategory && <span>{log.swimCategory}</span>}
+          </div>
         </div>
       </div>
-      <div className="rounded-md border border-gray-200 bg-gray-200 p-4 text-sm">
-        {log.note}
+      <div className="flex flex-col gap-2 rounded-md border border-gray-200 bg-gray-200 p-4 text-sm">
+        <span className="text-base font-semibold">오늘의 수영일지</span>
+        <p className="text-sm"> {log.note}</p>
       </div>
     </div>
   )
