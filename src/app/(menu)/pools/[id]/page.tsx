1e8041019e343a93d614a7e5cc9683c825d81e09
Chaesunbak <coanstjd0422@gmail.com>
2025-01-31 16:09:37 +0900

    refactor : 수영장 개별 조회 페이지 메타데이터 생성 로직 변경

--- src/app/(menu)/pools/[id]/page.tsx
+++ src/app/(menu)/pools/[id]/page.tsx
@@ -4,78 +4,40 @@ import {
   QueryClient,
 } from '@tanstack/react-query'
 import PoolPage from '@/components/pages/pool-page'
-import { getPool } from '@/actions/get-pool'
-import type { Metadata, ResolvingMetadata } from 'next'
+import type { Metadata } from 'next'
+import type { Pool } from '@/types/pool'
 import { notFound } from 'next/navigation'
+import { getPool } from '@/actions/pool'
 
-type Props = {
-  params: Promise<{ id: string }>
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string }
+}): Promise<Metadata> {
+  const poolId = Number(params.id)
+  const queryClient = new QueryClient()
 
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata,
-): Promise<Metadata> {
   try {
-    const id = (await params).id
-    const response = await fetch(`https://nest-aws.site/api/v1/pools/${id}`, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include',
+    await queryClient.prefetchQuery({
+      queryKey: ['pool', poolId],
+      queryFn: () => getPool(poolId),
     })
-
-    if (!response.ok) {
-      if (response.status === 404) {
-        notFound()
-      }
-    }
-
-    const data = await response.json()
-    const pool = data.data?.[0]
+    const pool = queryClient.getQueryData<Pool>(['pool', poolId])
 
     if (!pool) {
       notFound()
     }
 
-    const previousImages = (await parent).openGraph?.images || []
-
     return {
       title: pool.name,
       description: pool.description || '수영장 상세 정보',
       openGraph: {
-        images: [...previousImages],
         title: pool.name,
         description: pool.description || '수영장 상세 정보',
       },
     }
   } catch (error) {
     throw error
-  }
-}
-
-interface PrefetchDataProps {
-  queryClient: QueryClient
-  poolId: number
-  queryKey: string
-  queryFn: (id: number) => void
-}
-
-async function prefetchData({
-  queryClient,
-  poolId,
-  queryKey,
-  queryFn,
-}: PrefetchDataProps) {
-  try {
-    await queryClient.prefetchQuery({
-      queryKey: [queryKey, poolId],
-      queryFn: () => queryFn(poolId),
-    })
-  } catch (error) {
-    console.log(error)
-    return <div>에러 발생</div>
   }
 }
 
@@ -87,11 +49,9 @@ export default async function Pool({
   const poolId = Number((await params).id)
   const queryClient = new QueryClient()
 
-  await prefetchData({
-    queryClient,
-    poolId,
-    queryKey: 'pool',
-    queryFn: getPool,
+  queryClient.prefetchQuery({
+    queryKey: ['pool', poolId],
+    queryFn: () => getPool(poolId),
   })
 
   return (
