ae71d2b93c4a932751c0d193ad7bca138d74c142
Chaesunbak <coanstjd0422@gmail.com>
2025-02-03 18:47:32 +0900

    feat : 북마크 이미지 크기 올바르게, 이미지 없는 경우도 처리

--- src/components/pool/pool-bookmark-preview-item.tsx
+++ src/components/pool/pool-bookmark-preview-item.tsx
@@ -8,6 +8,7 @@ import {
 } from '@/components/ui/card'
 import Image from 'next/image'
 import { BookmarkI } from '@/hooks/use-my-bookmarks'
+import NoImage from '@/components/common/no-image'
 
 interface PoolBookmarkPreviewItemProps {
   bookmark: BookmarkI
@@ -19,14 +20,18 @@ export default function PoolBookmarkPreviewItem({
   return (
     <Link href={`/pools/${bookmark.pool.id}`} key={bookmark.id}>
       <Card className="mt-4 min-w-[300px] p-0 transition hover:opacity-90">
-        <CardHeader className="p-0">
-          <Image
-            src={bookmark.pool.img_url || 'https://picsum.photos/600/400'}
-            alt={bookmark.pool.name}
-            width={300}
-            height={200}
-            className="rounded-t-lg"
-          />
+        <CardHeader className="relative h-[200px] p-0">
+          {bookmark.pool.img_url ? (
+            <Image
+              src={bookmark.pool.img_url}
+              alt={bookmark.pool.name}
+              fill
+              className="rounded-t-lg object-cover"
+              sizes="(max-width: 300px) 100vw, 300px"
+            />
+          ) : (
+            <NoImage className="h-full w-full rounded-t-lg text-sm" />
+          )}
         </CardHeader>
         <CardContent className="flex flex-col gap-2 p-4">
           <CardTitle>{bookmark.pool.name}</CardTitle>
