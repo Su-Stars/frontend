4ff12d9c6b982d69d2dd6c2297429541fa252748
Chaesunbak <coanstjd0422@gmail.com>
2025-02-05 22:08:09 +0900

    fix : 모달 창 사용후 다시 열리시 않는 버그 수정

--- src/app/(admin)/admin/_components/data-table-row-actions.tsx
+++ src/app/(admin)/admin/_components/data-table-row-actions.tsx
@@ -1,17 +1,19 @@
 'use client'
 
-import { useState } from 'react'
-import IconMenu from '@/components/common/icon-menu'
-import { ResponsiveDialog } from '@/components/responsive-dialog'
 import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 import { Row } from '@tanstack/react-table'
+import { useToast } from '@/hooks/use-toast'
+import EditForm from '@/app/(admin)/admin/_components/edit-form'
+import { LuTrash2, LuNotebookPen } from 'react-icons/lu'
+import { useQueryClient } from '@tanstack/react-query'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
 import {
   AlertDialog,
   AlertDialogAction,
@@ -21,11 +23,8 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog'
-import { useToast } from '@/hooks/use-toast'
-import EditForm from '@/app/(admin)/admin/_components/edit-form'
-import { LuEllipsis, LuTrash2, LuNotebookPen } from 'react-icons/lu'
-import { useQueryClient } from '@tanstack/react-query'
 
 interface WithId<T> {
   id: number
@@ -38,16 +37,15 @@ interface DataTableRowActionsProps<TData> {
 export function DataTableRowActions<TData extends WithId<string>>({
   row,
 }: DataTableRowActionsProps<TData>) {
-  const [isEditOpen, setIsEditOpen] = useState(false)
-  const [isDeleteOpen, setIsDeleteOpen] = useState(false)
   const poolId = row.original.id
   const { toast } = useToast()
-  const [loading, setLoading] = useState(false)
   const queryClient = useQueryClient()
 
   // Handle the delete action
   const handleDelete = async (poolId: number) => {
-    setLoading(true)
+    if (!confirm('정말로 삭제하시겠습니까?')) {
+      return
+    }
     try {
       const response = await fetch(
         `https://nest-aws.site/api/v1/pools/${poolId}`,
@@ -74,91 +72,61 @@ export function DataTableRowActions<TData extends WithId<string>>({
         title: '수영장 삭제 완료',
         description: '해당 수영장이 삭제되었습니다.',
       })
-
-      // Close the dialog
-      setIsDeleteOpen(false)
     } catch (error) {
       toast({
         variant: 'destructive',
         title: '수영장 삭제 실패',
         description: (error as Error).message,
       })
-    } finally {
-      setLoading(false)
     }
   }
   return (
     <>
-      <ResponsiveDialog
-        isOpen={isEditOpen}
-        setIsOpen={setIsEditOpen}
-        title="수영장 정보 수정하기"
-      >
-        <EditForm poolId={Number(poolId)} setIsOpen={setIsEditOpen} />
-      </ResponsiveDialog>
-      <AlertDialog open={isDeleteOpen} onOpenChange={setIsDeleteOpen}>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline" aria-label="수정하기">
+            <LuNotebookPen aria-hidden />
+            <span className="sr-only">수정하기</span>
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle></DialogTitle>
+            <DialogDescription>수영장 수정하기</DialogDescription>
+            <DialogContent>
+              <EditForm poolId={poolId} />
+            </DialogContent>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="outline" aria-label="삭제하기">
+            <LuTrash2 aria-hidden />
+            <span className="sr-only">삭제하기</span>
+          </Button>
+        </AlertDialogTrigger>
         <AlertDialogContent>
           <AlertDialogHeader>
-            <AlertDialogTitle>수영장 삭제</AlertDialogTitle>
+            <AlertDialogTitle>수영장 삭제하기</AlertDialogTitle>
             <AlertDialogDescription>
-              정말로 삭제하시겠습니까? 이 작업은 되돌릴 수 없습니다.
+              이 행동은 되돌릴수 없습니다. 정말 삭제하시겠습니까?
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
             <AlertDialogCancel>취소</AlertDialogCancel>
-            <AlertDialogAction
-              className="bg-destructive text-destructive-foreground hover:bg-destructive"
-              onClick={() => {
-                handleDelete(Number(poolId))
-              }}
-            >
-              삭제하기
+            <AlertDialogAction asChild>
+              <Button
+                variant="destructive"
+                className="bg-red-500 hover:bg-red-600"
+                onClick={() => handleDelete(poolId)}
+              >
+                삭제하기
+              </Button>
             </AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
-
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            variant="ghost"
-            size="icon"
-            className="h-8 w-8 rounded-full p-0"
-          >
-            <span className="sr-only">메뉴 열기</span>
-            <LuEllipsis className="h-5 w-5" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end" className="z-50 w-[160px]">
-          <DropdownMenuItem className="font-base group flex w-full items-center justify-between p-0 text-left text-sm text-neutral-500">
-            <button
-              onClick={() => {
-                setIsEditOpen(true)
-              }}
-              className="flex w-full justify-start rounded-md p-2 transition-all duration-75 hover:bg-neutral-100"
-            >
-              <IconMenu
-                text="수정하기"
-                icon={<LuNotebookPen className="h-4 w-4" />}
-              />
-            </button>
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem className="font-base group flex w-full items-center justify-between p-0 text-left text-sm text-neutral-500">
-            <button
-              onClick={() => {
-                setIsDeleteOpen(true)
-              }}
-              className="flex w-full justify-start rounded-md p-2 text-red-500 transition-all duration-75 hover:bg-neutral-100"
-            >
-              <IconMenu
-                text="삭제하기"
-                icon={<LuTrash2 className="h-4 w-4" />}
-              />
-            </button>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
     </>
   )
 }
